6644667d54a68ef76e632c64c8941b03320f0d23
David Fonseca <fonsecasoundcruz@gmail.com>
2022-05-06 16:50:25 -0500

    Add project files.

---
+++ WebApiAngularADAzure/Controllers/ReportController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Web.Resource;
+using System;
+
+namespace WebApiAngularADAzure.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [RequiredScope(RequiredScopesConfigurationKey = "AzureAd:scopes")]
+    public class ReportController : ControllerBase
+    {
+
+        [Authorize(Roles = "Manager")]
+        [HttpGet("[action]")]
+        public IActionResult GetReport()
+        {
+            return File(System.IO.File.ReadAllBytes(@"C:\Tutorials\Styles.pdf"), "application/pdf");
+        }
+
+        [Authorize]
+        [HttpGet("[action]")]
+        public IActionResult GetReportStatus()
+        {
+            return Ok(new { Status = @"Report Generated at - " + DateTime.Now.ToString("MM-dd-yyyy HH:mm:ss") });
+        }
+             
+    }
+}
